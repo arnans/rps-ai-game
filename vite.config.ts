cf9c6cf91a62858bc77c06199dc1a2860bd91d48
Arnan Sipitakiat <arnans@gmail.com>
2026-01-14 21:21:11 +0700

    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 // Replace 'YOUR_REPO_NAME' with your actual repository name if not deploying to a custom domain
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/.', // Resolves @/ to the project root
+    },
+  },
   base: './', 
   build: {
     outDir: 'dist',
